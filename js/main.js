239f76b91d419549acdb4e7908c770b342a5b09e
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2023-12-20 15:02:02 +0100

    end milestone 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+/*
+CONSEGNA
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html,
+ stampiamo i post del nostro feed.
+Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e
+ incrementiamo il counter dei likes relativo.
+Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+
+SVOLGIMENTO
+MILESTONE2
+1)prendo in input il container
+2)leggo l'array di oggetti
+    -ad ogni lettura stampo nel DOM la struttura html con relative modifice prese dall'oggetto
+    tramite la proprità innerHTML
+MINLESTONE3
+1)prendo in input il pulsante "Mi piace"
+2)all'evento click aggiungo la classe che gli fa cambiare colore 
+3)incremento il counter dei likes
+    -prendo in input il counter
+    -modifico il counter
+    -stampo il counter
+4)Aggiungo tramite push gli elemnti a cui è stato messo like
+
+*/
+//array che contiene le informazioni da stampare nel DOM
 const posts = [
     {
         "id": 1,
@@ -55,3 +81,59 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+//input container
+let objPost = document.getElementById("container");
+console.log("objPost continete:", objPost, typeof objPost)
+
+//fine array di oggetti
+for (let i = 0; i < posts.length; i++) {
+    let userPhoto;
+    if (posts[i].author.image == null){
+       userPhoto = document.createElement("div");
+       userPhoto.classList.add("standardIcon");
+       userPhoto.append(posts[i].author.name[0]);
+    }
+    else{
+        userPhoto = document.createElement("img");
+        userPhoto.classList.add("profile-pic");
+        userPhoto.src = posts[i].author.image;
+        userPhoto.alt = posts[i].author.name;
+    }
+    console.log(i,"userPhoto continene", userPhoto, typeof userPhoto)
+    objPost.innerHTML +=`
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                                     
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${posts[i].author.name}</div>
+                    <div class="post-meta__time">${posts[i].created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${posts[i].content}</div>
+        <div class="post__image">
+            <img src="${posts[i].media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+   `;
+   const postMetaIcon = document.querySelector(".post:last-child .post-meta__icon");
+   postMetaIcon.append(userPhoto)
+   console.log(i,"post meta icon contine",postMetaIcon, typeof postMetaIcon);
+
+}
